d375256a45ceda33997ebc74cbde73f006bbc152
Your Name <olivertravis554@gmail.com>
2024-04-09 22:39:41 -0400

    logic to get, update, and delete decks

--- backend/routes/deckRoutes.js
+++ backend/routes/deckRoutes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { getDecks, createDecks } = require('../controllers/deckController');
+const {
+	getDecks,
+	getDeck,
+	createDecks,
+	updateDeck,
+	deleteDeck,
+} = require('../controllers/deckController');
 
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getDecks).post(protect, createDecks);
+
+router
+	.route('/:id')
+	.get(protect, getDeck)
+	.delete(protect, deleteDeck)
+	.put(protect, updateDeck);
 
 module.exports = router;
